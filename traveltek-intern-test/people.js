4cbdb837e5c79fea73b8de2976015a4dd69a8076
Smithd216 <smithd216@protonmail.com>
2018-05-03 20:23:03 +0100

    Completed all requirements except the eiffel tower and optional stat.

--- traveltek-intern-test/people.js
+++ traveltek-intern-test/people.js
@@ -16,15 +16,16 @@ function loadJSON(callback) {
 function loadPage() {
     loadJSON(function (response) {
         people_data = JSON.parse(response);
-        countEyeColour(people_data, "green");
+        countEyeColour(people_data, "green", true);
         surnameCount(people_data);
         //This second parameter is an array of the eiffel towers latitude and longitude
-        closestTo(people_data,[48.858370,2.294481])
+        closestTo(people_data, [48.858370, 2.294481]);
+        ageEyeColour(people_data, "blue");
     });
 }
 
 //Made eye colour a parameter for extensibility
-function countEyeColour(data, colour) {
+function countEyeColour(data, colour, display=false) {
     let counter = 0;
     //Simple for loop that runs through the list of people and tallies the target value
     for (let i = 0; i < data.length; i++) {
@@ -32,6 +33,8 @@ function countEyeColour(data, colour) {
             counter++;
         }
     }
+    display ? document.getElementById('eye-counter').innerHTML = `There are ${counter} people with ${colour} eyes.` : counter;
+    return counter;
 }
 
 function surnameCount(data) {
@@ -46,11 +49,24 @@ function surnameCount(data) {
         }
     }
     //Here we sort the counter objects and add them to an array
-    //In all honesty I have trouble following the logic in this form
-    keysSorted = Object.keys(counter).sort(function(a,b){return counter[a]-counter[b]})
-    
+    keysSorted = Object.keys(counter).sort(function (a, b) { return counter[a] - counter[b] })
+    document.getElementById('common-surnames').innerHTML = `The most common surnames are: <br> ${keysSorted.slice(-10)} <br> Although most of the surnames appeared 9 or 10 times each so there wasn't really a "top 10 most common."`;
 }
 
-function closestTo(data,target){
-    
+function closestTo(data, target) {
+    //I tried looking for implementations of finding the closest co-ordinates but the solutions were too complex for me.
+    //I could have copied them but that doesn't give an accurate representation of my abilities.
+    document.getElementById('closest-to').innerHTML = `I wasn't able to complete the eiffel tower part of the test.`;
+}
+
+//After looking at this functionality I would give the eye colour matching loop it's own function if I were to rewrite this
+function ageEyeColour(data, target) {
+    let age = 0;
+    for (let i = 0; i < data.length; i++) {
+        if (data[i].eyeColor == target) {
+            age += data[i].age;
+        }
+    }
+    average = age / countEyeColour(data, target); 
+    document.getElementById('average-eyes').innerHTML = `The average age of people with ${target} eyes is ${Math.round(average * 10)/10} .`;
 }
