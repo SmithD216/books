e6439888c9ae87a7effd440662973e0e73f5aa6a
Smithd216 <smithd216@protonmail.com>
2018-05-12 21:58:03 +0100

    Structuring project and learning components.

---
+++ text-adventure/src/components/app/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import "../../css/App.scss";
+import GC from "../../components/containers/GC";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <GC />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ text-adventure/src/components/containers/GC.js
@@ -0,0 +1,12 @@
+// components/containers/GC.js
+import React, { Component } from "react";
+
+export default class ComponentB extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello,</h1>
+            </div>
+        )
+    }
+}
