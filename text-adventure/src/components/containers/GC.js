48084483df3f9cc72d70e1b1cec0041120fd9dc7
Smithd216 <smithd216@protonmail.com>
2018-05-12 22:03:27 +0100

    Centering game container.

--- text-adventure/src/components/containers/GC.js
+++ text-adventure/src/components/containers/GC.js
@@ -4,8 +4,8 @@ import React, { Component } from "react";
 export default class ComponentB extends Component {
     render() {
         return (
-            <div>
-                <h1>Hello,</h1>
+            <div class="game-container">
+                <h1>Hello</h1>
             </div>
         )
     }
