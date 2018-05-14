cb557eadc480751ff2b14b5a07b5f25451d32c6b
Smithd216 <smithd216@protonmail.com>
2018-05-14 17:54:47 +0100

    Made buttons clickable.

--- text-adventure/src/components/logic/ListButtons.js
+++ text-adventure/src/components/logic/ListButtons.js
@@ -1,19 +1,19 @@
 import React, { Component } from "react";
 import Button from "../controls/Button";
+import player from "../logic/player";
 import text from "../story/ButtonText";
 
 function createList(content){
     const buttonList = content.map((item) =>
         <Button key={item.toString()} text={item}/>
     );
-
     return(buttonList);
 }
 
 export default class ListButtons extends Component {
     render() {
         return (
-            <div class="button-container">{createList(text(0))}</div>
+            <div class="button-container">{createList(text(player.status))}</div>
         )
     }
 }
---
+++ text-adventure/src/components/logic/player.js
@@ -0,0 +1,7 @@
+let player = {
+    status:0,
+    name:"Boots McSuits",
+    health:100
+};
+
+export default player;
